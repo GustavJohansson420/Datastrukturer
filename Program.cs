d185cd3c0c4ec4e51049418f7518efd3340b6896
Bob156590 <gujo04002@utb.vaxjo.se>
2023-01-12 12:32:02 +0100

    stack

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿string input = "[()]{}{[()()]()}";
+Stack<string> stack = new Stack<string>();
+
+stack.Push("[");
+stack.Push("(");
+stack.Push(")");
+stack.Push("]");
+stack.Push("{");
+stack.Push("}");
+stack.Push("{");
+stack.Push("[");
+stack.Push("(");
+stack.Push(")");
+stack.Push("(");
+stack.Push(")");
+stack.Push("]");
+stack.Push("(");
+stack.Push(")");
+stack.Push("}");
